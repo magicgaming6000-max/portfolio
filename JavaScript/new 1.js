b29a491a44d37719868a722d7e61be8256f6c519
magicgaming6000 <magicgaming6000@gmail.com>
2025-12-31 15:25:12 -0600

    Sticky nav element added but broken hamburger menu

--- JavaScript/new 1.js
+++ JavaScript/new 1.js
@@ -1,9 +1,17 @@
-function toggleMenu() {
-    var menu = document.getElementById("mobile-menu");
-
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
-}
+<header>
+    <nav class="navbar">
+        <a href="#" class="nav-branding">Logo</a>
+        <ul class="nav-menu">
+            <li class="nav-item"><a href="#">Home</a></li>
+            <li class="nav-item"><a href="#">About</a></li>
+            <li class="nav-item"><a href="#">Contact</a></li>
+			<li class="nav-item"><a href="#">trips</a></li>
+        </ul>
+        <div class="hamburger">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+			<span class="bar"></span>
+        </div>
+    </nav>
+</header>
